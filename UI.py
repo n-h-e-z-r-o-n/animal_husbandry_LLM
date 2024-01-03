f8685990ce67b419d209b02e0a18ac56fff61603
Hezron Na <hezron.w12@gmail.com>
2024-01-03 14:44:13 +0300

    Committed 2024 0We.144413

--- UI.py
+++ UI.py
@@ -63,7 +63,7 @@ def Request_Info(user_query):
         VIEW_BOX.config(state=tk.DISABLED)
 
 
-        QUERY_BT.config(state=tk.DISABLED)
+        QUERY_BT.config( text = "", state=tk.DISABLED)
         answer = "ejfioewgewogwegkoewpglewgewgg"
         #answer = RAG.LLM_Run(str(user_query))
 
