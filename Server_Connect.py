179cab1d23a1fe0da641bc6264629ae3ffd2b63f
Hezron Na <hezron.w12@gmail.com>
2024-01-03 14:06:54 +0300

    Committed 2024 0We.140654

--- Server_Connect.py
+++ Server_Connect.py
@@ -9,48 +9,17 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = "127.0.0.1"
 port = 22575
+
 server_socket.bind((host, port))
 active_servers = []
 active_users = []
 server_socket.listen(5)  # Listen for incoming connections
 
-
 print(f"Server listening on {host}:{port}")
 
 
 def handle_user_connection(socket_object):
-    while True:
-        Query = socket_object.recv(1024).decode("utf-8")
-        print("===", Query)
-        if Query != "":
-            print("Query: ", Query)
-            Hold = []
-
-            for i in active_servers:
-                i.sendall(str(Query).encode('utf-8'))
-                result = i.recv(1024).decode("utf-8")
-                Hold.append(result)
-
-            if len(Hold) != 0:
-                index = random.randint(0, (len(Hold)-1))
-                socket_object.sendall(str(Hold[index]).encode('utf-8'))
-            else:
-                socket_object.sendall(str("None").encode('utf-8'))
-
-        elif Query == '':
-            print()
-            socket_object.close()
-            break
-    return
-
-
-def handle_server_connection(socket_object):
-    while True:
-        ready_to_read, _, _ = select.select([socket_object], [], [], 0)
-        if ready_to_read:
-            print("Server disconnected")
-            active_servers.remove(socket_object)
-            break
+    pass
 
 
 def main():
