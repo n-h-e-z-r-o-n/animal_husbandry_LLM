7898a0c602dbecdabd0d7bca8e59392a13bc03d0
Hezron Na <hezron.w12@gmail.com>
2024-01-03 13:29:07 +0300

    Committed 2024 0We.132907

--- train.py
+++ train.py
@@ -10,14 +10,15 @@ from langchain.chains import LLMChain
 from langchain.llms import GradientLLM
 from langchain.prompts import PromptTemplate
 
+# Set the environment variables for gradient.ai using your access token and workspace ID
 os.environ['GRADIENT_ACCESS_TOKEN'] = "1vhvNKf2lLAMPvrOqhV97xaPPNRzwT1J"
 os.environ['GRADIENT_WORKSPACE_ID'] = "b04a475d-65d1-4e98-82f2-62d218be3989_workspace"
 
 
 # ========================== LOADING THE DATASET AND DIVIDING IT INTO CHUNKS =====================================================
-def divide_into_chunks(number, chunk_size):
-    chunks = []
 
+def divide_into_chunks(number, chunk_size): # Define a function to divide a number into chunks of a given size
+    chunks = []
     while number > 0:
         if number >= chunk_size:
             chunks.append(chunk_size)
@@ -29,14 +30,15 @@ def divide_into_chunks(number, chunk_size):
     return chunks
 
 
-with open("/content/data.json") as f:
+with open("/content/data.json") as f: # Load the dataset from a JSON file
     samples = json.load(f)
 
-chunks = divide_into_chunks(len(samples), 100)
+chunks = divide_into_chunks(len(samples), 100) # Divide the dataset into chunks of 100 samples each
 print(chunks)
 
 # =============================== FINE TUNING THE MODEL ============================================================================
 
+# Create a Gradient object to interact with gradient.ai
 with Gradient() as gradient:
     base_model = gradient.get_base_model(base_model_slug="nous-hermes2")
     new_model_adapter = base_model.create_model_adapter(name="Animal_husbandry-LLM(hezron)")
