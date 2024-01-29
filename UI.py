c29dddaee184ab2ab25c11e5cfe4a0cc47f01ec8
Hezron Na <hezron.w12@gmail.com>
2024-01-29 15:58:30 +0300

    Committed 2024 0Mo.155829

--- UI.py
+++ UI.py
@@ -52,7 +52,9 @@ def imagen(image_path, screen_width, screen_height, widget):
 
 
 def ask_binary_choice():
-    global CHANGE_LLM, model_no
+    global CHANGE_LLM, model_no, status
+    if status == 1:
+        return
     if model_no == 2:
         CHANGE_LLM.config(text="Fine_tuned only")
         model_no = 1
@@ -141,7 +143,7 @@ def main():
     # VIEW_DISPLAY, welcome_page_root = attach_scroll(VIEW_BOX)
 
     VIEW_BOX = tk.Text(VIEW_BOX_canvas, bg=bg_color, borderwidth=0, border=0, font=(13), wrap="word")
-    VIEW_BOX.place(relx=0, rely=0, relheight=1, relwidth=1)
+    #VIEW_BOX.place(relx=0, rely=0, relheight=1, relwidth=1)
     VIEW_BOX.tag_configure("user_config", foreground="#B2BEB5", justify=tk.LEFT)  # user queries  config's
     VIEW_BOX.tag_configure("llm_config", foreground="#54626F", justify=tk.LEFT)  # llm responses config's
     VIEW_BOX.config(state=tk.DISABLED)
