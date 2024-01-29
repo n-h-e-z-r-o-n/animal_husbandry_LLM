7444f24c29d6efcac3d9608591d6a5020512bda1
Hezron Na <hezron.w12@gmail.com>
2024-01-29 16:15:16 +0300

    Committed 2024 0Mo.161515

--- UI.py
+++ UI.py
@@ -138,9 +138,10 @@ def main():
     #app.wm_attributes("-transparentcolor", "blue")
     imagen(r"C:\Users\HEZRON WEKESA\Downloads\pexels-matthis-volquardsen-2157000.jpg", 950, 700, BACKGROUND)
 
-    VIEW_BOX_canvas = tk.Frame(app, bg=bg_color, borderwidth=0, border=0)
+    VIEW_BOX_canvas = tk.Label(app, bg=bg_color, borderwidth=0, border=0)
     VIEW_BOX_canvas.place(relx=0.05, rely=0.1, relheight=0.7, relwidth=0.9)
-    # VIEW_DISPLAY, welcome_page_root = attach_scroll(VIEW_BOX)
+    imagen(r"C:\Users\HEZRON WEKESA\Downloads\pexels-matthis-volquardsen-2157000.jpg", int(950*0.9),int(700*), VIEW_BOX_canvas)
+
 
     VIEW_BOX = tk.Text(VIEW_BOX_canvas, bg=bg_color, borderwidth=0, border=0, font=(13), wrap="word")
     VIEW_BOX.place(relx=0, rely=0, relheight=1, relwidth=1)
